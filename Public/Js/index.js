c6ef342aefceada5fec96373977e6be90a401b60
DuvanArwenLazar <3dtuningduvan@gmail.com>
2023-01-30 21:20:02 -0500

    JavaScript Added, Little Function now works

---
+++ Public/Js/index.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+    document.getElementById("s-title").innerHTML == "Concentración" ? document.getElementById("s-title").innerHTML = "Enfoque" : document.getElementById("s-title").innerHTML = "Concentración";
+}, 30000);
