c6ffe53ffcc6087c61c2696406d9e98b5b3d5cf0
DuvanArwenLazar <3dtuningduvan@gmail.com>
2023-02-11 16:48:57 -0500

    HTML, CSS & JS - Frontend

--- Public/Js/signup.js
+++ Public/Js/signup.js
@@ -1,12 +1,8 @@
 function email_validate(){
+    
     email = document.getElementById("email_u");
     confirm_email = document.getElementById("email_confirm_u");
-
-    if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
-        // 
-    }
-
-    if(email.value != confirm_email.value){
-        // 
-    }
+    
 }
+
+// -------------------------------------------- event listener ---------------------------------------------------- //
